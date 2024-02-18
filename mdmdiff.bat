18ca6d416d897bbaee4e4e5af771a49561983be8
Andrey Putilov <Andrey.Putilov@materialplus.io>
2024-02-18 03:16:24 -0800

    add 32 as a section number for MDM properties

--- mdmdiff.bat
+++ mdmdiff.bat
@@ -25,7 +25,7 @@ SET "REPORT_A=report.%MDD_A%.json"
 SET "REPORT_B=report.%MDD_B%.json"
 REM :: possible params:
 REM ::  "/a:RUN_FEATURES=label,properties,translations,scripting"
-REM ::  /d:RUN_SECTIONS=23
+REM ::  /d:RUN_SECTIONS=55
 REM ::  "/a:RUN_FEATURES_LABEL_FORCE_CONTEXT=Question"
 REM ::  "/a:RUN_FEATURES_PROPERTIES_CONTEXTS=Analysis,Question"
 mrscriptcl mdmrep.mrs "/a:INPUT_MDD=%MDD_A%" "/a:RUN_FEATURES=label,properties,translations"
@@ -34,7 +34,7 @@ python mdmcreatehtmlrep.py "%REPORT_A%"
 if %ERRORLEVEL% NEQ 0 ( echo ERROR: Failure && pause && exit /b %errorlevel% )
 REM :: possible params:
 REM ::  "/a:RUN_FEATURES=label,properties,translations,scripting"
-REM ::  /d:RUN_SECTIONS=23
+REM ::  /d:RUN_SECTIONS=55
 REM ::  "/a:RUN_FEATURES_LABEL_FORCE_CONTEXT=Question"
 REM ::  "/a:RUN_FEATURES_PROPERTIES_CONTEXTS=Analysis,Question"
 ECHO "- Getting report for MDD_B %MDD_B%"
